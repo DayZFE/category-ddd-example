60d632c5f8e3f61869a1d8cce7426d81258c85c5
Song <song.limin@tenfen.com>
2022-02-28 12:05:48 +0800

    Initial commit

---
+++ src/main/java/org/dayzfe/category/CategoryDddExampleApplication.java
@@ -0,0 +1,13 @@
+package org.dayzfe.category;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryDddExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CategoryDddExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dayzfe/category/CategoryDddExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dayzfe.category;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CategoryDddExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
